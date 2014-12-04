4e2a9ad105c9cb7de2e8642350a9682d26cdb71b
annierose22 <nataliawojcik@college.harvard.edu>
2014-12-03 21:36:19 -0500

    adding js things

--- popup.js
+++ popup.js
@@ -1,3 +1,4 @@
+// pop-up has one button -when user clicks it, sends message to background script 
 window.onload = function() {
     document.getElementById("button").onclick = function() {
         chrome.extension.sendMessage({
