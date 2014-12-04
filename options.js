b18c27502d6fd3961fbf3573cf991063bc5d2a8d
annierose22 <nataliawojcik@college.harvard.edu>
2014-12-03 22:08:37 -0500

    figuring what goes where

--- options.js
+++ options.js
@@ -1,11 +1,12 @@
 // Save options to chrome.storage
+// should probably change color to color for font and color for background specifically 
 function save_options() {
 // figure out how to apply this to the preset colors and sliders?
   var color = document.getElementById('color').value;
   var backgroundColor = document.getElementById('    ').checked;
   chrome.storage.sync.set({
     Color: color,
-    
+
   }, function() {
     // Update status to let user know options were saved.
     var status = document.getElementById('status');
