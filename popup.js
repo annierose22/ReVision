1e912c207e6968023a953da8cab7304f031c65da
annierose22 <nataliawojcik@college.harvard.edu>
2014-12-03 22:59:47 -0500

    fixing the colors

--- popup.js
+++ popup.js
@@ -12,5 +12,18 @@ window.onload = function() {
         chrome.extension.sendMessage({
             type: "color-divs"
         });
-    }
+    };
+
+    $('#picker').colpick({
+    	layout:'hex',
+    	submit:0,
+    	colorScheme:'dark',
+    	onChange:function(hsb,hex,rgb,el,bySetColor) {
+    		$(el).css('border-color','#'+hex);
+    		// Fill the text box just if the color was set using the picker, and not the colpickSetColor function.
+    		if(!bySetColor) $(el).val(hex);
+    	}
+    }).keyup(function(){
+    	$(this).colpickSetColor(this.value);
+    });
 }
