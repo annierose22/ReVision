ff926817ef5c1e4f059c484689e59ec1926d6f13
annierose22 <nataliawojcik@college.harvard.edu>
2014-12-03 22:42:26 -0500

    .

--- README.md
+++ README.md
@@ -3,3 +3,10 @@ CS50Project
 
 CS50 dyslexia extension
 (should add full description later)
+
+// will definitely need this later but wasnt working when i tried to get button to appear
+"background": {
+  "scripts": "",
+  "persistent": true
+},
+"options_page": "",
--- popup.js
+++ popup.js
@@ -1,4 +1,12 @@
-// pop-up has one button -when user clicks it, sends message to background script 
+
+// Sets the html document to be opened as a popup when the user clicks on the browser action's icon
+chrome.browserAction.setPopup(object details) // add object details lol
+
+// actually gets the html document set as the popup for this browser action.
+// not sure if this should be in this file? think so though
+chrome.browserAction.getPopup(object details, function callback)
+
+// pop-up has one button -when user clicks it, sends message to background script
 window.onload = function() {
     document.getElementById("button").onclick = function() {
         chrome.extension.sendMessage({
