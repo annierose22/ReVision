a9cc6bb1e3a2bf547f42b2e46aa19058b3f75692
annierose22 <nataliawojcik@college.harvard.edu>
2014-12-05 19:12:36 -0500

    whoops

--- js/popup.js
+++ js/popup.js
@@ -11,21 +11,37 @@ chrome.browserAction.setIcon(object details, function callback)
 */
 // pop-up has one button -when user clicks it, sends message to background script
 window.onload = function() {
-    document.getElementById("button").onclick = function() {
-        chrome.extension.sendMessage({
-            type: "color-divs"
-        });
-    };
+    // document.getElementById("button").onclick = function() {
+    //     chrome.extension.sendMessage({
+    //         type: "color-divs"
+    //     });
+    // };
 
     $('#picker').colpick({
+      flat:true,
     	layout:'hex',
     	submit:0,
     	colorScheme:'dark',
     	onChange:function(hsb,hex,rgb,el,bySetColor) {
-    		$(el).css('border-color','#'+hex);
-    		// Fill the text box just if the color was set using the picker, and not the colpickSetColor function.
-    		if(!bySetColor) $(el).val(hex);
+    		chrome.tabs.executeScript({
+          code: 'document.body.style.color = "#' + hex + '"'
+        });
     	}
     }).keyup(function(){
     	$(this).colpickSetColor(this.value);
     });
+
+    $("#fontForm").submit(function() {
+      var font = $("#fontselect").val();
+
+      // document.getElementsByTagName("p");
+      /*slect p tag and iterate over it
+      element.style.fontFamily = "' + font + '"*/
+
+      chrome.tabs.executeScript({
+        code: 'document.body.style.fontFamily = "' + font + '"'
+      });
+
+      return false;
+    });
+};
