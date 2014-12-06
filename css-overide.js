0c4b000108c7c699c38ca23799ff3c894adffb67
annierose22 <nataliawojcik@college.harvard.edu>
2014-12-05 21:01:16 -0500

    deletions

--- css-overide.js
+++
@@ -1,9 +0,0 @@
-// inject the css file into the head element of website
-function appendStyleNode(id, href) {
-    var cssNode = document.createElement('link');
-    cssNode.type = 'text/css';
-    cssNode.rel = 'stylesheet';
-    cssNode.id = id;
-    cssNode.href = href;
-    document.getElementsByTagName('head')[0].appendChild(cssNode);
-}
